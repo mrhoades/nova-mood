a7ca59cdb861d77cdef745c33baccb8c76d7a19f
matt.rhoades <mrhoades@hp.com>
2013-10-01 18:41:13 -0700

    generate graph - take one

--- graphs/failure_rates_by_region.py
+++ graphs/failure_rates_by_region.py
@@ -1,39 +1,93 @@
 import pygal
 from decimal import Decimal
+from dateutil import rrule
 import nova_mood_db
+from datetime import datetime
+from datetime import timedelta
+
+numdays = 100
+base = datetime.today()
+dateList = [base - timedelta(days=x) for x in range(0, numdays)]
+mydatelist = list(rrule.rrule(rrule.DAILY, count=100, dtstart=datetime.now()))
+
+print mydatelist
+print dateList
+pass
 
 
-def expo_scaling_avg_time_to_x(environ_name, last_x_minutes):
-    """ time to boot, active, ssh, total - try using data in stacked bar chart """
+def failure_rates_by_zone(bool_prettytable=False):
+    """ get failure counts, rate, grouped by day, for each zone """
 
     sql_query = """
-    select count(*) as total_nodes,
-      concurrency_count,
-      AVG(time_total) as avg_time_total,
-      AVG(time_to_active) as avg_time_to_active,
-      AVG(time_to_ping) as avg_time_to_ping,
-      AVG(time_to_ssh) as avg_time_to_ssh,
-      AVG(hard_errors_exist) as avg_error
-    from test_results
-    group by concurrency_count
-    order by concurrency_count;
-    """.format(environ_name, str(last_x_minutes))
+    select DATE_FORMAT(t.time_started, '%m-%d-%Y') as my_date,
+      tp.environ_name,
+      tp.zone,
+      count(*) as total_tests,
+      SUM(hard_errors_exist) as total_failures,
+      AVG(t.hard_errors_exist) as failure_rate
+    from test_results as t
+    join test_passes as tp on tp.test_pass_id = t.test_pass_id
+    where tp.environ_name = 'bravo'
+     and tp.time_started > DATE_SUB(NOW(), INTERVAL 40 day)
+    group by my_date, tp.zone
+    order by my_date;
+    """
 
-    result = nova_mood_db.exec_query(sql_query)
+    result = nova_mood_db.exec_query(sql_query, bool_prettytable)
 
     return result
 
 
+sql_result_data = failure_rates_by_zone(bool_prettytable=True)
 
-# stacked_bar = pygal.StackedBar(rounded_bars=10)
-# stacked_bar.title = 'Browser usage evolution (in %)'
-# stacked_bar.x_labels = map(str, range(2002, 2013))
-# stacked_bar.add('Firefox', [None, None, 0, 16.6,   25,   31, 36.4, 45.5, 46.3, 42.8, 37.1])
-# stacked_bar.add('Chrome',  [None, None, None, None, None, None,    0,  3.9, 10.8, 23.8, 35.3])
-# stacked_bar.add('IE',      [85.8, 84.6, 84.7, 74.5,   66, 58.6, 54.7, 44.8, 36.2, 26.6, 20.1])
-# stacked_bar.add('Others',  [14.2, 15.4, 15.3,  8.9,    9, 10.4,  8.9,  5.8,  6.7,  6.8,  7.5])
-# stacked_bar.render_to_file('stacked_bar_chart.svg')
-# stacked_bar.render_to_png('stacked_bar_chart.png')
+print sql_result_data
+
+
+sql_result_data = failure_rates_by_zone()
+
+# plot_data = []
+#
+# # get labels
+# date_labels = list(set(x[0] for x in sql_result_data))
+# zone_labels = set(x[2] for x in sql_result_data)
+#
+#
+# # sort the date labels
+# date_labels.sort(key=lambda x: datetime.strptime(x, '%m-%d-%Y'))
+
+az1_data = []
+az2_data = []
+az3_data = []
+
+for index, row in enumerate(sql_result_data):
+
+    date_object = datetime.strptime(row[0], '%m-%d-%Y')
+
+    if row[1] == 'bravo' and row[2] == 'az1':
+        az1_data.append([date_object, int(row[5] * 100)])
+    elif row[1] == 'bravo' and row[2] == 'az2':
+        az2_data.append([date_object, int(row[5] * 100)])
+    elif row[1] == 'bravo' and row[2] == 'az3':
+        az3_data.append([date_object, int(row[5] * 100)])
+
+from pygal.style import NeonStyle
+chart = pygal.DateY(style=NeonStyle,
+                    tooltip_border_radius=10,
+                    width=800,
+                    height=600,
+                    x_label_rotation=45,
+                    y_label_rotation=30,
+                    truncate_label=12,
+                    show_dots=True,
+                    dots_size=1)
+
+chart.title = 'Bravo Failure Rate by Zone'
+chart.add('useast-az1', az1_data)
+chart.add('useast-az3', az2_data)
+chart.add('useast-az3', az3_data)
+chart.render_to_file('line_chart.svg')
+chart.render_to_png('line_chart.png')
+
 
 
 
